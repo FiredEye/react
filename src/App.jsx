ccaf2a1445f45671c77c8db393af2c0641468684
Rashik Tuladhar <rashiktuladhar13@gmail.com>
2023-10-09 22:48:29 +0545

    fetching external data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react"
-import Child from "./pages/Child";
+import UseApi from "./hook/UseApi"
 
 function App() {
-  const [add,setAdd]=useState(0);
-  const [sub,setSub]=useState(100);
-  let value="Hi! there"
-  useEffect(()=>{console.log(`after effect activated ${add}, ${sub}`)},[add,sub])
-  return (
-    <> 
-    <h1 className="text-orange-400 text-center text-[40px]">React hooks:</h1>
+  const paragraphStyles = {
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  WebkitLineClamp: 3,
+};
+const [data,load,error]  =UseApi('https://www.themealdb.com/api/json/v1/1/categories.php')
+if(load) return <div className="h-[100vh] w-full flex justify-center items-center"><h1 className=" mt-4 text-yellow-500 text-[40px]">Loading...</h1></div>
+if(error) return<div className="h-[100vh] w-full flex justify-center items-center"><h1 className=" mt-4 text-red-500 text-center text-[40px]">Something went wrong...</h1></div>
 
-    <div className="ps-8 pt-8">
-    <h1 className="text-purple-500 text-[30px]">useState:</h1>
-    <p>Press the button to increase the number</p> <b>{add}</b> <button className="px-4 py-2 bg-green-500 text-white rounded ms-3" onClick={()=>{setAdd(prev=>prev+1)}}>increment</button><br />
-    
-    <p>Press the button to decrease the number</p> <b>{sub}</b> <button className="px-4 py-2 bg-red-500 text-white rounded ms-3" onClick={()=>{setSub(prev=>prev-1)}}>decrement</button>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-center text-purple-500 text-[40px] my-4">Available Dishes:</h1>
+      {data && data.categories.map((cata, i) => {
+        return <div key={cata.idCategory} className="flex justify-center items-center flex-col bg-gray-100 rounded mb-[30px] shadow-md p-[20px] w-[500px] gap-3">
+          <h1>{cata.strCategory}</h1>
+          <img src={cata.strCategoryThumb} alt="dish image" />
+          <p className="overflow-hidden max-h-20 leading-5 text-ellipsis" style={paragraphStyles}>{cata.strCategoryDescription}</p>
+        </div>
+        
+
+        
+      })}
+
     </div>
-    <div className="ps-8 pt-8">
-    <h1 className="text-purple-500 text-[30px]">sending Props in children component:</h1>
-    <p>Using value:"{value}" in parent component</p> <br />
-    {/* sending props(value) to children component */}
-    <Child value={value}/>
-    </div>
-    </>
+
   )
 }
 
